3428d732f2ffee14bbfb6be44586b824f294c517
barakdaniel <barakdaniel22@gmail.com>
2022-05-02 18:15:07 +0300

    minor changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 import { PlatformComponent } from './platform/platform.component';
 
 const routes = [
-  {path: '', component: PlatformComponent},
-  
+  { path: '', component: PlatformComponent },
+  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
